5d255257da9d7dc7532cd9cf0fe5ee30a83c2b88
Epilogut1364 <wanglu_1998927@outlook.com>
2021-09-10 18:04:51 +0800

    输出转置

--- excelProcessMeanZhuanzhi.m
+++ excelProcessMeanZhuanzhi.m
@@ -2,7 +2,7 @@
 %用于绘图，看每个齿宽中，最大ΔB所对应的槽宽
 clear;
 clc;
-file_name = ['D:\OneDrive\桌面\resultPythonTest.xlsx'];
+file_name = ['D:\OneDrive\桌面\resultMean.xlsx'];
 fid = fopen(file_name, 'r');%打开要读取的文件
 jxw0=0.4;%槽宽初始值
 cw0=0.12;%齿宽初始值
@@ -14,33 +14,34 @@ jxwcell=2;%用于输出结果时的坐标
 cwcell=2;%用于输出结果时的坐标
 list_cell = {};%储存
 output=[];
-list_data=xlsread(file_name, 1, 'A2:I793');%将数据一同从excel中读出
+list_data=xlsread(file_name, 1, 'A2:I793')%将数据一同从excel中读出
 %因为读取数据所需要的运算量比较大，如果分步读运行会非常慢
 for jxw=jxw0:s1:jxw1
-    output(jxhcell,1)=jxh;
+    output(jxwcell,1)=jxw;
     jxw=round(jxw*100)/100;%为保证对比时精度相同
     for cw=cw0:s2:cw1
-        cw=round(cw*000)/100;
+        output(1,cwcell)=cw;
+        cw=round(cw*100)/100;
         for c1=1:1:792
             result=round(list_data(c1,3)*100)/100;
             if result==jxw
                 result=round(list_data(c1,4)*100)/100;
                 if result==cw
-                    result=round(list_data(c1,9)*10000)/10000;
-                    list_cell = [list_cell;result];
+                    result=round(list_data(c1,9)*100)/100;
+                    list_cell = [list_cell;result]
                 end
             end
         end
         list_cell = cell2mat(list_cell);
-        resultmax=mean(list_cell);
-        output(jxwcell,cwcell)=resultmax;
+        resultmean=mean(list_cell)
+        output(jxwcell,cwcell)=resultmean;
         cwcell=cwcell+1;
         list_cell = {};
         output;
     end
-    cwcell=1;
+    cwcell=2;
     jxwcell=jxwcell+1;
     output 
 end
-xlswrite(file_name, output, 2, 'B2') %输出结果为output的转置，为了后面分析方便而做的更改，该前面的输出顺序太麻烦，所以直接转置
+xlswrite(file_name, output', 4, 'A1') %输出结果为output的转置，为了后面分析方便而做的更改，该前面的输出顺序太麻烦，所以直接转置
 fclose(fid);
