ae30229968684b7036cc71a758209587f3cc993f
Epilogut1364 <wanglu_1998927@outlook.com>
2021-10-18 17:07:39 +0800

    更新

--- excelProcessjxhMeanZhuanzhi.m
+++ excelProcessjxhMeanZhuanzhi.m
@@ -2,19 +2,19 @@
 %用于绘图，看每个齿宽中，最大ΔB所对应的槽宽
 clear;
 clc;
-file_name = ['D:\OneDrive\桌面\resultMean.xlsx'];
+file_name = ['D:\GitHub\paper1\result2Cr13\jxh0.3-1-jxw0.4-1.4-cw0.12-0.28normB.xlsx'];
 fid = fopen(file_name, 'r');%打开要读取的文件
 jxh0=0.3;%槽高初始值
 cw0=0.12;%齿宽初始值
 jxh1=1.0;%槽高最大值
 cw1=0.28;%齿宽最大值
 s1=0.1;
-s2=0.02;
+s2=0.04;
 jxhcell=2;%用于输出结果时的坐标
 cwcell=2;%用于输出结果时的坐标
 list_cell = {};%储存
 output=[];
-list_data=xlsread(file_name, 1, 'A2:I793');%将数据一同从excel中读出
+list_data=xlsread(file_name, 2, 'A2:I441');%将数据一同从excel中读出
 %因为读取数据所需要的运算量比较大，如果分步读运行会非常慢
 for jxh=jxh0:s1:jxh1
     output(jxhcell,1)=jxh;
@@ -22,7 +22,7 @@ for jxh=jxh0:s1:jxh1
     for cw=cw0:s2:cw1 
         output(1,cwcell)=cw;
         cw=round(cw*100)/100;
-        for c1=1:1:792
+        for c1=1:1:440
             result=round(list_data(c1,2)*100)/100;
             if result==jxh
                 result=round(list_data(c1,4)*100)/100;
@@ -43,5 +43,5 @@ for jxh=jxh0:s1:jxh1
     jxhcell=jxhcell+1;
     output 
 end
-xlswrite(file_name, output', 5, 'A1') %输出结果为output的转置，为了后面分析方便而做的更改，该前面的输出顺序太麻烦，所以直接转置
+xlswrite(file_name, output', 4, 'A1') %输出结果为output的转置，为了后面分析方便而做的更改，该前面的输出顺序太麻烦，所以直接转置
 fclose(fid);
--- excelProcessjxhjxwMax.m
+++ excelProcessjxhjxwMax.m
@@ -2,7 +2,7 @@
 %用于绘图，看每个齿宽中，最大ΔB所对应的槽宽
 clear;
 clc;
-file_name = ['E:\GitHub\paper1\resultMAX.xlsx'];
+file_name = ['D:\GitHub\paper1\result2Cr13\jxh0.3-1-jxw0.4-1.4-cw0.12-0.28normB.xlsx'];
 fid = fopen(file_name, 'r');%打开要读取的文件
 % jxh=槽高，jxw=槽宽，cw=极齿宽，jx=间隙，cnz=总槽数，cln=左槽数，crn右槽数
 %设置变量下限
@@ -14,12 +14,12 @@ jxh1=1.0;%%槽高范围0.3-1.0mm
 jxw1=1.4;%%槽宽范围0.4-1.4mm
 cw1=0.28;%%极齿宽范围0.12-0.28mm
 s1=0.1;
-s2=0.02;
+s2=0.04;
 jxwcell=2;%用于输出结果时的坐标
 cwcell=2;%用于输出结果时的坐标
 list_cell = {};%储存
 output=[12,9];
-list_data=xlsread(file_name, 1, 'A2:I793')%将数据一同从excel中读出
+list_data=xlsread(file_name, 2, 'A2:I441')%将数据一同从excel中读出
 %因为读取数据所需要的运算量比较大，如果分步读运行会非常慢
 for jxw=jxw0:s1:jxw1
     output(jxwcell,1)=jxw;
@@ -27,7 +27,7 @@ for jxw=jxw0:s1:jxw1
     for jxh=jxh0:s1:jxh1
         output(1,cwcell)=jxh;
         jxh=round(jxh*100)/100;
-        for c1=1:1:792
+        for c1=1:1:440
             result=round(list_data(c1,3)*100)/100;
             if result==jxw
                 result=round(list_data(c1,2)*100)/100;
@@ -46,7 +46,7 @@ for jxw=jxw0:s1:jxw1
     end
     cwcell=2;
     jxwcell=jxwcell+1;
-    output 
+    output; 
 end
-xlswrite(file_name, output, 6, 'A1') %输出结果为output的转置，为了后面分析方便而做的更改，该前面的输出顺序太麻烦，所以直接转置
+xlswrite(file_name, output, 5, 'A1') %输出结果为output的转置，为了后面分析方便而做的更改，该前面的输出顺序太麻烦，所以直接转置
 fclose(fid);
